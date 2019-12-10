fb930e79974a007dc6fff7eba21a79ae23799f7e
Brandon Dyer <brandondyer64@gmail.com>
2019-12-10 15:47:20 -0600

    More proposed syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::any::Any;
 
 pub struct State {
     map: HashMap<&'static str, StateUnit>,
@@ -41,8 +42,40 @@ macro_rules! component {
     };
 }
 
+#[macro_export]
+macro_rules! com {
+    (
+        < $name:ident $($prop_name:ident = ( $prop_value:expr ) )* > {
+            $($child:expr)*
+        }
+    ) => {
+        {
+            let children: Vec<dyn Any> = Vec::new();
+            $(children.push($child);)*
+            $name {
+                $($prop_name: $prop_value),*
+            }.render(use_state!(StateUnit::SubState(State::new(None))), children)
+        }
+    };
+    (
+        < $name:ident $($prop_name:ident = ( $prop_value:expr ) )* / >
+    ) => {
+        com!(< $name $($prop_name = ( $prop_value ))* > { })
+    }
+}
+
+
 component!(App(bob: i32) => {
-    
+    let (bananas, set_bananas) = use_state!(42);
+
+    com!(<Div> {
+        com!(<Div bob=(11)> {
+            format!("Bananas: {}", bananas)
+        })
+        com!(<Div onclick=(|_| set_bananas(|b| b + 1))> {
+            "Add banana"
+        })
+    })
 });
 
 #[test]
@@ -51,3 +84,12 @@ fn test() {
         bob: 17,
     }.render(State::new(None), Vec::new());
 }
+
+// Renders as
+/*
+<div>
+    bob: 17
+    bananas: 42
+    <div onclick="...">Click to add bananas</div>
+</div>
+*/
